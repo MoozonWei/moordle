b7afe54bd78f61321532265dee619125d00f4d3f
魏子涵 <muzhenweizh@gmail.com>
2022-03-12 20:46:49 +0800

    change url

--- src/components/Statistics.vue
+++ src/components/Statistics.vue
@@ -94,7 +94,7 @@ const text = computed(() => `Moordle\n${
       }
     }).join('')
   }).join('\n')
-}\n[${store.gameState === 'w' ? (store.boardCurRow  + '/6') : '-_-'}]·${dayjs().format('YYYY-MM-DD')}\nmoordle.netlify.app`)
+}\n[${store.gameState === 'w' ? (store.boardCurRow  + '/6') : '-_-'}]·${dayjs().format('YYYY-MM-DD')}\nhttps://moordle.netlify.app/`)
 
 const share = useShare(computed(() => ({
   title: 'Moordle',
